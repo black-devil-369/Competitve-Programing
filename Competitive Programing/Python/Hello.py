d441188f66ca58c540fe74618c3d969c735258c7
black-devil-369 <ajaychauhan1262@gmail.com>
2022-06-02 18:08:35 -0400

    Competitive_Coding

---
+++ Competitive Programing/Python/Hello.py
@@ -0,0 +1,11 @@
+#print("Hello Wrold")
+"""num = int(input("Enter your Number\n"))
+print("Number is Entered By You\n",num)"""
+
+# multiple input in pyhon
+# by Split() and list comprehension method
+
+x ,y= input("Enter the Number of boys and girls in School\n").split()
+print("Number of boys:\n",x)
+print("Number of girls:\n",y)
+print()
