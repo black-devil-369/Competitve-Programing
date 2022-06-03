f3405989ee1b3e3660524c803de642bfb0938fc0
black-devil-369 <ajaychauhan1262@gmail.com>
2022-06-03 05:08:15 -0400

    Coding

---
+++ Competitive Programing/Python/string.py
@@ -0,0 +1,18 @@
+# String
+string = 'Welcome to Github'
+print('String use from Single quota')
+print(string)
+
+string1= "I am Ajay Chauhan"
+print("String used from Double quota")
+print(string1)
+
+string1 = '''I am Competitive Coder'''
+print('''String used from Triple quota''')
+print(string1)
+
+string1 = ''' Hiii
+Github
+Welcome to World
+'''
+print(string1)
