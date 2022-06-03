b5e134c614b3e00d07c7970452544de48a654f5f
black-devil-369 <ajaychauhan1262@gmail.com>
2022-06-03 05:40:30 -0400

    Coding

--- Competitive Programing/Python/string.py
+++ Competitive Programing/Python/string.py
@@ -16,3 +16,26 @@ Github
 Welcome to World
 '''
 print(string1)
+
+# Formatting of String
+
+string1 = " {} {} {} ".format("Welcome","to", "Github")
+print("Print String in the same Order")
+print(string)
+
+#Positinal Formatting
+string1 = "{1} {2} {0}".format("Welcome","to","Github")
+print("Print String in this Order")
+print(string1)
+
+#keyword Formatting
+string1 = "{a} {b} {c}".format(a="Welcome", b="coding", c="World")
+print("Print a String by keyword formatting")
+print(string1)
+
+# Old Style Formatting
+Integer1 = 12.3456789
+print("Formatting in 3.2f format: ")
+print('The value of Integer1 is %3.2f' % Integer1)
+print("\nFormatting in 3.4f format: ")
+print('The value of Integer1 is %3.4f' % Integer1)
