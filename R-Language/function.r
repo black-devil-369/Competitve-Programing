ace6c1a764c1f62ffdec3382c1caf681dfaf363a
black-devil-369 <ajaychauhan1262@gmail.com>
2022-08-07 18:49:38 -0400

    fuction

--- R-Language/function.r
+++ R-Language/function.r
@@ -4,5 +4,16 @@ evenOdd = function(x){
     else
     return ("Odd")
 }
+divisibleby5 <- function(n){
+    if(n%%5==0){
+        return("n is divisible by 5")
+    }
+    else{
+        return("n is not divisible by 5")
+    }
+
+}
 print(evenOdd(5))
 print(evenOdd(6))
+divisibleby5(100)
+divisibleby5(53)
