3ad444206d00b2da3e13d9d093f5b0403aab4e51
black-devil-369 <ajaychauhan1262@gmail.com>
2022-08-07 18:44:07 -0400

    practice set

--- C/1.c
+++ C/1.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
 int main(){
     int n;
-    scanf("Enter the value of :%d",&d)
+    printf("Enter the number for star printing:\n");
+    scanf("%i",&n);
+    printf("Printed star according by you enter number \n");
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    printf(" \n");
+    printf("Printing a star in another form\n");
+    for(int i = 1; i<=n; i++){
+        for(int j =n; j>=i; j--){
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("Printing a number in triange form\n");
+    for(int i = 1; i<=n; i++){
+        for(int j = n; j>=i; j--){
+            printf(" %i ", j);
+        }
+        printf("\n");
+    }
+    return 0;
 }
