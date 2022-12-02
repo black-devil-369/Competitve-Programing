f165feea7773e6c235a8847bc455131ac0b1c75d
black-devil-369 <ajaychauhan1262@gmail.com>
2022-12-02 02:21:37 -0500

    numpy1

---
+++ Python/Numpy/ndarrays.py
@@ -0,0 +1,14 @@
+import numpy as ac # numpy is used for delecaring a N dimensional Arrays
+aja = ac.array([[34,53],
+                [67,35],
+                [74,15]])
+#printing the type of arrays;
+print("Arrays type of ",type(aja))
+#printing the dimesion of arrays
+print("No of dimension is",aja.ndim)
+#printing the shape of arrays
+print("The size of arrays",aja.shape)
+#printing the total element of arrays is
+print("The no of Element of arrays",aja.size)
+#printing the type of element in arrays
+print("The type of element is",aja.dtype)
