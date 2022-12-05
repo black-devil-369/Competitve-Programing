d303f6d759ee25b36f7e20195f08fa2babe831d4
black-devil-369 <ajaychauhan1262@gmail.com>
2022-12-05 10:32:50 -0500

    loop rule

---
+++ Javascript/Basic/Loop.js
@@ -0,0 +1,11 @@
+// do-while loop
+let a = 1;
+do{
+    console.log(a);
+    a++;
+}while(a<=10);
+// for loop 
+console.log("Printing the number by help of for loop");
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
