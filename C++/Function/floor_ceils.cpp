23f8c2ca62342d64aa9216113b87b220984b2877
black-devil-369 <ajaychauhan1262@gmail.com>
2022-12-09 14:02:36 -0500

    ceils build function

---
+++ C++/Function/floor_ceils.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //int x = 3.9;
+    //int y = -3.9;
+    cout<<"The floor x : "<<floor(3.9)<<endl;
+    cout<<"The floor y : "<<floor(-3.9)<<endl;
+    cout<<"The ceil x is : "<<ceil(3.9)<<endl;
+    cout<<"The ceil y is : "<<ceil(-3.9)<<endl;
+    return 0;
+}
