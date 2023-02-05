58c02a59880e38a7168567f0c916f00ba1faeb96
black-devil-369 <ajaychauhan1262@gmail.com>
2023-02-05 08:07:33 -0500

    class method in python

--- Python/OOPs concept/class.py
+++ Python/OOPs concept/class.py
@@ -10,7 +10,7 @@ p.printName()
 
 #Poloymorphism in python 
 # Polymorhpism with python class
-'''''
+
 class India():
     def capital(self):
         print("Delhi is capital of India")
@@ -29,34 +29,6 @@ class USA():
 obj_usa = USA()
 obj_ind = India()
 for country in (obj_ind,obj_usa):
-    country.capital()
-    country.language()
-    country.type()
-
-'''
-class India():
-    def capital(self):
-        print("New Delhi is the capital of India.")
- 
-    def language(self):
-        print("Hindi is the most widely spoken language of India.")
- 
-    def type(self):
-        print("India is a developing country.")
- 
-class USA():
-    def capital(self):
-        print("Washington, D.C. is the capital of USA.")
- 
-    def language(self):
-        print("English is the primary language of USA.")
- 
-    def type(self):
-        print("USA is a developed country.")
- 
-obj_ind = India()
-obj_usa = USA()
-for country in (obj_ind, obj_usa):
     country.capital()
     country.language()
     country.type()
