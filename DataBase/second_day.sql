32b17836ebbdec9456bb1ab2cbf40c5928628568
black-devil-369 <ajaychauhan1262@gmail.com>
2023-02-06 09:12:30 -0500

    insert a data inside a sql database system

---
+++ DataBase/second_day.sql
@@ -0,0 +1,22 @@
+CREATE TABLE College
+(
+    First_Name  VARCHAR(225),
+    Last_Name VARCHAR(225),
+    Year_ VARCHAR(226),
+    Course VARCHAR(226),
+    Branch VARCHAR(225),
+    Email VARCHAR(226),
+    Phone_No INT(10),
+
+);
+INSERT INTO College VALUES
+("Ajay","Chauhan","3rd","B.tech","CSE","ajaychauhan1262@gmail.com",9648068163),
+("Aditya","Singh","3rd","B.tech","CSE","adityasingh3432@gmail.com",6346324755),
+("Abhishek","kanaujiya","3rd","B.tech","CSE","abhishekkanajiya3432@gmail.com",7434567424),
+("Manashivi","Jaiswal","3rd","B.tech","CSE",",manashivijaiswal6434@gmail.com",6342742256),
+("Avinash","Pandey","3rd","B.tech","Civil","avinashpandey1363@gmail.com",9342462123),
+("Vikash","Modanwal","2rd","B.tech","Civil","vikashmodanwal6434@gmail.com",8953249826),
+("Akash","Kumar","1st","Diploma","Electrical","akashkumar4321@gmail.com",8342312533),
+("Vikash","Chauhan","4th","B.tech","CSE","vikashchauhan6342@gmail.com",9844243243),
+("Ausutosh","Yadava","3rd","B.Pharm","Medical","ausutoshyadav3342@gamail.com",9893424642)
+;
