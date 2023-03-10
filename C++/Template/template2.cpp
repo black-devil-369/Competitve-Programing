30383d5970f2950c5124023a3317d0930f49ed53
black-devil-369 <ajaychauhan1262@gmail.com>
2023-03-09 22:23:44 -0500

    process to creating a template in c++

--- C++/Template/template2.cpp
+++ C++/Template/template2.cpp
@@ -4,6 +4,21 @@ using namespace std;
 x product(x a,x b){
     return a*b;
 }*/
+template<class y>
+class Char{
+    y a;
+    y b;
+    public:
+    Char(y,y);
+    y obtainedvalue();
+};
+template<class y>Char<y>::Char(y d,y e){
+    this->a = d;
+    this->b = e;
+}
+template<class y> y Char<y>::obtainedvalue(){
+    return a;
+}
 template<class x>
 class Add{
     x a;
@@ -23,8 +38,12 @@ int main(){
     ios_base::sync_with_stdio(NULL);
     Add<float>a1(6.0,3.8);
     Add<float>a2(7.0,9);
+
     cout<<a1.getvalue()<<endl;
     cout<<a2.getvalue()<<endl;
+   // cout<<a3.getvalue()<<endl;
+    Char<char>c1('A','B');
+    cout<<c1.obtainedvalue()<<endl;
     //cout<<product(3.2,5.6);
     return 0;
 }
