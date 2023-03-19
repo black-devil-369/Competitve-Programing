7f7795ca74050b913226ce24ee24e6242cc0c853
black-devil-369 <ajaychauhan1262@gmail.com>
2023-03-18 20:58:20 -0400

    arrays ds in Csharp

---
+++ C#/DSA/ADT.cs
@@ -0,0 +1,15 @@
+using System;
+namespace ADT{
+    class Arrays{
+        private int capacity;
+        private int lastIndex;
+        private int *ptr;
+        // creating a constructor
+        public Arrays(int cap){
+            capacity = cap;
+            lastIndex = -1;
+            ptr = new int[cap];
+        }
+        public static void Main(String[] args)
+    }
+}
