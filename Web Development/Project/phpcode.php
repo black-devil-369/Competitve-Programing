624ecf568ad13f6c795119bdf3ee68fec78fafc1
black-devil-369 <ajaychauhan1262@gmail.com>
2023-03-19 12:12:07 -0400

    php pratice

---
+++ Web Development/Project/phpcode.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title style="color:blue;">
+            Welcome to webpage
+        </title>
+    </head>
+    <body>
+        <h1 style="align-content:center;">Gurukul University</h1>
+    </body>
+</html>
