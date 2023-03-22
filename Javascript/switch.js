1bb1c5292811c9b077451f40706ece3e028295e7
black-devil-369 <ajaychauhan1262@gmail.com>
2023-03-22 13:47:43 -0400

    switch case pratice in js

---
+++ Javascript/switch.js
@@ -0,0 +1,21 @@
+let c = "b"
+switch(c){
+    case "a":
+        console.log("A")
+        break;
+    case "b":
+        console.log("B")
+        break
+    case "c":
+        console.log("C")
+        break
+    case "d":
+        console.log("D")
+        break
+    case "e":
+        console.log("E")
+        break
+    default:
+        console.log("NO")
+        break;
+}
