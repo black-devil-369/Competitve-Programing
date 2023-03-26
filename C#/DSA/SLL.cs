9dd9f9cfcbe83fa7ecc6e9dceb43edf6afc6a430
black-devil-369 <ajaychauhan1262@gmail.com>
2023-03-26 18:01:52 -0400

    Singly LinkedList pratice in C#

--- C#/DSA/SLL.cs
+++ C#/DSA/SLL.cs
@@ -1,8 +1,8 @@
 using System;
 namespace LinkedList{
     public struct Node{
-        public int item;
-        public Node next;
+        public int item{get;set;}
+        public Node next{get;set;}
     }
     class SLL{
          private Node start;
@@ -44,14 +44,118 @@ namespace LinkedList{
                 t=t.next;
             }
         }
-    }
-    // Derive class
+    }// end class 
+    
     class Program{
         public static void Main(String[] args){
-            SLL s = new SLL();
+           /* SLL s = new SLL();
             s.insertAtbegin(56);
             s.insertAtEnd(43);
             s.PrintList();
+            */
+            LinkedList mylist = new LinkedList();
+            mylist.Add(43);
+            mylist.Add(46);
+            mylist.Add(89);
+            mylist.Print();
         }
     }
 }
+
+// Another code for linkedlist without using generic code
+class Node 
+{
+        public int data;
+        public Node next;
+        public Node(int d) {
+            data = d;
+            next = null;
+        }
+}
+    class LinkedList {
+        Node head;
+        public void AddNode(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+            }
+            else {
+                Node current = head;
+                while (current.next != null) {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+        public void PrintList() {
+            Node current = head;
+            while (current != null) {
+                Console.Write(current.data + " ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+    // Another class for linkedlist code
+    public class LinkedListNode
+    {
+        public int Data { get; set; }
+        public LinkedListNode Next { get; set; }
+        public LinkedListNode(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+    public class LinkedList
+    {
+        private LinkedListNode head;
+        public LinkedList()
+        {
+            head = null;
+        }
+        public void Add(int data)
+        {
+            LinkedListNode newNode = new LinkedListNode(data);
+            if (head == null){
+                head =newNode;
+            }
+            else{
+                LinkedListNode current = head;
+                while (current.Next != null){
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+        public void Remove(int data){
+            if (head == null){
+                return;
+            }
+            if (head.Data == data){
+                head = head.Next;
+                return;
+            }
+            LinkedListNode current = head;
+            while (current.Next != null){
+                if (current.Next.Data == data){
+                    current.Next = current.Next.Next;
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+        public void Print(){
+            LinkedListNode current = head;
+            while (current != null){
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+// linkedlist program without using generic code
+
+
+
