f489dff0adc2c29b2e6485fe5bd1d8e09831eced
black-devil-369 <ajaychauhan1262@gmail.com>
2023-04-06 20:30:00 -0400

    Doubly linkedlist data structure using java

---
+++ Java/DSA/DoublyLinkedlist.java
@@ -0,0 +1,67 @@
+class Node{
+    public Node prev;
+    public int item;
+    public Node next;
+}
+class DoublylinkedList{
+    private Node start;
+    public DoublylinkedList(){
+        start = null;
+    }
+    public void insertAtbegin(int data){
+        Node n = new Node();
+        n.item=data;
+        n.prev=null;
+        if(start==null){
+            n.next=start;
+            start=n;
+        }
+        else{
+            n.next =start;
+            start.prev = n;
+            start =n;
+        }
+        // another way to reduce the line of above code is
+        /*Node n = new Node();
+        n.prev =null;
+        n.item=data;
+        n.prev=null;
+        if(start!=null){
+            start.prev=n;
+        }
+        start =n;
+        */
+    }
+    public void insertAtEnd(int data){
+        Node n = new Node();
+        n.item =data;
+        n.next = null;
+        if(start==null){
+            n.next = null;
+            start=n;
+        }
+        else{
+            Node t=start;
+            while(t.next!=null){
+                t=t.next;
+            }
+            n.prev=t;
+            t.next=n;
+        }
+    }
+    public void printlist(){
+        Node t;
+        t=start;
+        while(t!=null){
+            System.out.print(t.item+" ");
+            t=t.next;
+        }
+        System.out.println(" ");
+    }
+}
+// creating a Deriver Program to run the code
+class DoublyLinkedlist{
+    public static void main(String[] args){
+        DoublyLinkedlist d = new DoublyLinkedlist();
+    }
+}
