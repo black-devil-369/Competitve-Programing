97d85b48c716e62109210f2a8b1c6a989f64eab0
black-devil-369 <ajaychauhan1262@gmail.com>
2023-04-11 19:26:18 -0400

    Recursion pratice in C#

---
+++ C#/RecursionProblem/NNaturalnumber.cs
@@ -0,0 +1,40 @@
+using System;
+namespace NNaturalnumber{
+    // recursive function for N natural number
+    class Recursion{
+        public void Nnatural(int nums){
+            if(nums>0){
+                Nnatural(nums-1);
+                Console.WriteLine(nums);
+            }
+        }
+        // recursive functino for Reverse N Natural number
+        public void RNNatural(int num){
+            if(num>0){
+                Console.WriteLine(num);
+                RNNatural(num-1);
+            }
+        }
+        // recursive function for N odd Natural number;
+        public void NODD(int k){
+            if(k>0){
+                NODD(k-1);
+                Console.WriteLine(2*k-1);
+            }
+        }
+        public void RNODD(int k){
+            if(k>0){
+                Console.WriteLine(2*k-1);
+                RNODD(k-1);
+            }
+        }
+    }
+    class Deriver{
+        public static void Main(String[] args){
+            Recursion r = new Recursion();
+            Console.WriteLine("The N natural number");
+            r.Nnatural(4);
+            r.NODD(10);
+        }
+    }
+}
