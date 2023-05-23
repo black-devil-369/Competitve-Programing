8e912e0f2bcffcd90ab2810e5630bf70d53254b0
black-devil-369 <ajaychauhan1262@gmail.com>
2023-05-23 11:49:13 -0400

    using of object keywords in csharp

--- C#1/Fundamentals/1_day.cs
+++ C#1/Fundamentals/1_day.cs
@@ -4,7 +4,9 @@ namespace Boxing{
     class Program{
         public static void Main(){
             int i = 10;
+            // boxing the variable or value to reference variable
             object b = i;
+            // Unboxing the referenced variable into a value
             int f = (int)b;
             Console.WriteLine(i);
             Console.WriteLine(b);
